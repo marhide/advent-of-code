f382756b0d0829eb1b22e2c5b66b6883311f4ca1
L S <ls@louis-mac.lan>
2025-03-29 13:47:12 +0000

    refact 5.1

--- src/05_print_queue.py
+++ src/05_print_queue.py
@@ -1,42 +1,32 @@
 with open('data/05_input.txt', 'r', encoding='utf-8') as f:
     order_list = [l.strip() for l in f if '|' in l]
-    order_tuple_list = [(l.split('|')[0], l.split('|')[1]) for l in order_list]
     order_dict = {l.split('|')[0]: [] for l in order_list}
+    order_pair_tuple_list = [(l.split('|')[0], l.split('|')[1]) for l in order_list]
 
-    for item in order_tuple_list:
-        if item[0] in order_dict:
-            order_dict[item[0]].append(item[1])
+    for pair in order_pair_tuple_list:
+        if pair[0] in order_dict:
+            order_dict[pair[0]].append(pair[1])
 
 with open('data/05_input.txt', 'r', encoding='utf-8') as f:
-    updates_list = [l.strip() for l in f if ',' in l]
+    updates_list = [l.strip().split(',') for l in f if ',' in l]
 
-# print(order_dict)
-# print(updates_list)
-
-
-correct_updates = []
+correct_middle_pages = []
 for update in updates_list:
     pages_checked = []
     is_update_correct = True
-    for page in update.split(','):
+    for page in update:
         if page in order_dict:
+            pages_checked.append(page)
             for incorrect_page in order_dict[page]:
                 if incorrect_page in pages_checked:
+                    # print(f'{", ".join(update)} is incorrect: {incorrect_page} cannot come before {page}')
                     is_update_correct = False
-                    print(f'{update} is incorrect, {incorrect_page} cannot come before {page}')
-            pages_checked.append(page)
-        if not is_update_correct:
-            break
-                
-    if is_update_correct:
-        correct_updates.append(update)
-        print(f'{update} is correct')
-    
-middle_pages = []
-for update in correct_updates:
-    update_list = update.split(',')
-    middle_index = int(len(update_list)/2)
-    middle_pages.append(int(update_list[middle_index]))
-    
+                    break
+            if not is_update_correct:
+                break
 
-print(sum(middle_pages))
+    if is_update_correct:
+        middle_index = int(len(update)/2)
+        correct_middle_pages.append(int(update[middle_index]))
+    
+print(sum(correct_middle_pages))
