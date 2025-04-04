d49b1eb1ca394f45051702b18c09af85dc2b178e
L S <ls@louis-mac.lan>
2025-04-05 00:09:15 +0100

    part 5 done refact

--- src/05_print_queue.py
+++ src/05_print_queue.py
@@ -7,7 +7,6 @@ with open('data/05_input.txt', 'r', encoding='utf-8') as f:
         if pair[0] in order_dict:
             order_dict[pair[0]].append(pair[1])
         
-
 with open('data/05_input.txt', 'r', encoding='utf-8') as f:
     updates_list = [l.strip().split(',') for l in f if ',' in l]
 
@@ -27,42 +26,31 @@ def update_checker(update):
     return is_update_correct
 
 
-counter = 0
-def update_corrector(update):
-
-    global counter
+def update_corrector(update, counter=0):
 
     if update_checker(update) and counter:
-        # print (f"{update} is correct, {counter}")
-        counter = 0
         return update
 
     counter += 1
-
     pages_checked = []
+
     for page in update:
         pages_checked.append(page)
         if page in order_dict:
             for incorrect_page in order_dict[page]:
                 if incorrect_page in pages_checked:
-                    # print(f'{", ".join(update)} is incorrect: {incorrect_page} cannot come before {page}')
                     update.remove(incorrect_page)
                     update.append(incorrect_page)
-                    return update_corrector(update)
+                    return update_corrector(update, counter)
 
-
-get_middle_page = lambda update: int(update[int(len(update)/2)])
-
-def sum_middle_pages(updates_list_to_sum):
-    middle_pages = []
-    for update in updates_list_to_sum:
-        middle_page = get_middle_page(update)
-        middle_pages.append(middle_page)
-    return sum(middle_pages)
 
 correct_updates = filter(update_checker, updates_list)
-
 incorrect_updates = filter(lambda update: not update_checker(update), updates_list)
 corrected_updates = map(update_corrector, incorrect_updates)
+
+def sum_middle_pages(updates_list_to_sum):
+    get_middle_page = lambda update: int(update[int(len(update)/2)])
+    middle_pages = [get_middle_page(update) for update in updates_list_to_sum]
+    return sum(middle_pages)
 
 print(f'part 1: {sum_middle_pages(correct_updates)}\npart 2: {sum_middle_pages(corrected_updates)}')
