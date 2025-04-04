c007a5f1398ceed6c38bc00ca62def6027b0014f
L S <ls@louis-mac.lan>
2025-04-04 23:57:21 +0100

    5 p2 done

--- src/05_print_queue.py
+++ src/05_print_queue.py
@@ -6,12 +6,13 @@ with open('data/05_input.txt', 'r', encoding='utf-8') as f:
     for pair in order_pair_tuple_list:
         if pair[0] in order_dict:
             order_dict[pair[0]].append(pair[1])
+        
 
 with open('data/05_input.txt', 'r', encoding='utf-8') as f:
     updates_list = [l.strip().split(',') for l in f if ',' in l]
 
-correct_middle_pages = []
-for update in updates_list:
+
+def update_checker(update):
     pages_checked = []
     is_update_correct = True
     for page in update:
@@ -19,14 +20,49 @@ for update in updates_list:
             pages_checked.append(page)
             for incorrect_page in order_dict[page]:
                 if incorrect_page in pages_checked:
-                    # print(f'{", ".join(update)} is incorrect: {incorrect_page} cannot come before {page}')
                     is_update_correct = False
                     break
-            if not is_update_correct:
-                break
+        if not is_update_correct:
+            break
+    return is_update_correct
 
-    if is_update_correct:
-        middle_index = int(len(update)/2)
-        correct_middle_pages.append(int(update[middle_index]))
-    
-print(sum(correct_middle_pages))
+
+counter = 0
+def update_corrector(update):
+
+    global counter
+
+    if update_checker(update) and counter:
+        # print (f"{update} is correct, {counter}")
+        counter = 0
+        return update
+
+    counter += 1
+
+    pages_checked = []
+    for page in update:
+        pages_checked.append(page)
+        if page in order_dict:
+            for incorrect_page in order_dict[page]:
+                if incorrect_page in pages_checked:
+                    # print(f'{", ".join(update)} is incorrect: {incorrect_page} cannot come before {page}')
+                    update.remove(incorrect_page)
+                    update.append(incorrect_page)
+                    return update_corrector(update)
+
+
+get_middle_page = lambda update: int(update[int(len(update)/2)])
+
+def sum_middle_pages(updates_list_to_sum):
+    middle_pages = []
+    for update in updates_list_to_sum:
+        middle_page = get_middle_page(update)
+        middle_pages.append(middle_page)
+    return sum(middle_pages)
+
+correct_updates = filter(update_checker, updates_list)
+
+incorrect_updates = filter(lambda update: not update_checker(update), updates_list)
+corrected_updates = map(update_corrector, incorrect_updates)
+
+print(f'part 1: {sum_middle_pages(correct_updates)}\npart 2: {sum_middle_pages(corrected_updates)}')
