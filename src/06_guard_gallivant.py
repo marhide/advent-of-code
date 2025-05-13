fcf1607d0935341ca5cf8f34795bed9e73e7b406
marhide <louis.smith@hyperoceanic.com>
2025-05-13 02:29:54 +0100

    day 6 part 2 done

--- src/06_guard_gallivant.py
+++ src/06_guard_gallivant.py
@@ -1,3 +1,6 @@
+from copy import deepcopy
+
+
 def add_border_to_table(table):
     table = ['E'+row+'E' for row in table]
     end_row = ''.join(['E' for _ in range(len(table[0]))])
@@ -13,13 +16,14 @@ def get_starting_positions(table):
 
 
 def get_unique_position_count(table):
-
     table = add_border_to_table(table)
     col_index, row_index = get_starting_positions(table)
     coords_visited = []
     direction_index = 0
+    counter = 0
+    table_copy = deepcopy(table)
 
-    while table[col_index][row_index] != 'E':
+    while table[col_index][row_index] != 'E':  
         match direction_index:
             case 0:
                 col_index -= 1
@@ -46,16 +50,37 @@ def get_unique_position_count(table):
                 direction_index = 0
 
         coords_visited.append((col_index, row_index))
+        counter += 1
+        table_copy[col_index] = table_copy[col_index][:row_index]+"X"+table_copy[col_index][row_index+1:]
 
-    #     table[col_index] = table[col_index][:row_index]+"X"+table[col_index][row_index+1:]
+        if counter > 9999:
+            for row in table_copy: print(row)
+            return None
+
     # for row in table: print(row)
 
     unique_position_count = len(set(coords_visited))
     return unique_position_count
 
 
+
+def get_table_permuations(table):
+    table_permutations = []
+    for col_index in range(len(table)):
+        for row_index in range(len(table[col_index])):
+            if table[col_index][row_index] != '^':
+                table_copy = deepcopy(table)
+                table_copy[col_index] = table_copy[col_index][:row_index]+"#"+table_copy[col_index][row_index+1:]
+                table_permutations.append(table_copy)
+
+    return table_permutations
+
+
 if __name__ == '__main__':
     with open('data/06_input.txt', 'r', encoding='utf-8') as file:
         table = [line.strip() for line in file]
 
-    print(get_unique_position_count(table))
+    table_permuations = get_table_permuations(table)
+    looping_tables_count = len(list(filter(lambda table: not get_unique_position_count(table), table_permuations)))
+
+print(f'part 1: {get_unique_position_count(table)}\npart 2: {looping_tables_count}')
