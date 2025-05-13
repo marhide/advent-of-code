d88e6330e593d9265cddc50737d6422b1e4b6241
marhide <louis.smith@hyperoceanic.com>
2025-05-13 01:23:00 +0100

    day 6 part 1 done

---
+++ src/06_guard_gallivant.py
@@ -0,0 +1,59 @@
+def add_border_to_table(table):
+    table = ['E'+row+'E' for row in table]
+    end_row = ''.join(['E' for _ in range(len(table[0]))])
+    table = [end_row]+table+[end_row]
+    return table
+
+
+def get_starting_positions(table):
+    for col_index in range(len(table)):
+        for row_index in range(len(table[col_index])):
+            if table[col_index][row_index] == '^':
+                return col_index, row_index
+
+
+def get_unique_position_count(table):
+
+    table = add_border_to_table(table)
+    col_index, row_index = get_starting_positions(table)
+    coords_visited = []
+    i = 0
+
+    while table[col_index][row_index] != 'E':
+        if i == 0:
+            col_index -= 1
+        if i == 1:
+            row_index += 1
+        if i == 2:
+            col_index += 1
+        if i == 3:
+            row_index -= 1
+
+        if table[col_index][row_index] == '#':
+            if i == 0:
+                col_index += 1
+            if i == 1:
+                row_index -= 1
+            if i == 2:
+                col_index -= 1
+            if i == 3:
+                row_index += 1
+
+            i += 1
+            if i > 3:
+                i = 0
+
+        coords_visited.append((col_index, row_index))
+
+    #     table[col_index] = table[col_index][:row_index]+"X"+table[col_index][row_index+1:]
+    # for row in table: print(row)
+
+    unique_position_count = len(set(coords_visited))
+    return unique_position_count
+
+
+if __name__ == '__main__':
+    with open('data/06_input.txt', 'r', encoding='utf-8') as file:
+        table = [line.strip() for line in file]
+
+    print(get_unique_position_count(table))
