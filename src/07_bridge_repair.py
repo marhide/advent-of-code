217aca3a44df4255f142f245b0b02b22e9ab7907
marhide <louis.smith@hyperoceanic.com>
2025-05-16 00:33:54 +0100

    day 7 part 1 done

--- src/07_bridge_repair.py
+++ src/07_bridge_repair.py
@@ -1,65 +1,36 @@
-
-from pprint import pprint
-
-
-def get_all_operator_combinaton_list(maxLength, s=""):
-    if len(s) == maxLength:
-        return s
+def get_operator_sequence_permutations(number_of_sequences, current_sequence=''):
+    if len(current_sequence) == number_of_sequences:
+        return current_sequence
     else:
-        temp = get_all_operator_combinaton_list(maxLength, s + "+") + "\n"
-        temp += get_all_operator_combinaton_list(maxLength, s + "*")
-        return temp
+        output = get_operator_sequence_permutations(number_of_sequences, current_sequence+'+')+' '
+        output += get_operator_sequence_permutations(number_of_sequences, current_sequence+'*')
+        return output
     
-def check_if_equation_possible(item):
 
+def check_if_equation_possible(item):
+    equation_possible = False
     result = item[0]
     equation = item[1]
-    number_of_permuations = len(equation)**2
+    number_of_operators = len(equation)-1
 
+    operator_sequence_permutations = get_operator_sequence_permutations(number_of_operators).split(' ')
 
-
-    # print(equation, allBinaryPossiblities(len(equation)-1))
-
-    operator_list = get_all_operator_combinaton_list(len(equation)-1).split('\n')
-    print(equation, operator_list)
-
-    # operations_list.append(equation)
-    total = 0
-    for i in range(len(operator_list)):
+    for i in range(len(operator_sequence_permutations)):
         total = equation[0]
-        for j in range(len(equation[1::])):
-            if operator_list[i][j] == "+":
-                total += equation[j]
-            elif operator_list[i][j] == "*":
-                total *= equation[j]
+        operator_sequence = operator_sequence_permutations[i]
+
+        for j in range(number_of_operators):
+            operator = operator_sequence[j]
+            number = equation[j+1]
+            if operator == '+':
+                total += number
+            elif operator == '*':
+                total *= number
+
         if total == result:
-            print(total, result, number_of_permuations)
-            return True
-        
-    return False
+            equation_possible = True
 
-    # for i in range(number_of_permuations):
-    #     new_list = []
-    #     total = equation[0]
-    #     for j in range(len(equation)):
-            # if j:
-            #     if i%j:
-            #         new_list.append('0')
-            #         total += equation[j]
-            #     else: 
-            #         new_list.append('1')
-            #         total *= equation[j]
-
-        # new_list.append('   '+str(i))
-
-
-        # operations_list.append(' '.join(new_list))
-
-
-
-
-    # pprint(operations_list)
-    # print(equation, len(set(operations_list)), len(operations_list))
+    return equation_possible
 
 
 if __name__ == '__main__':
@@ -70,5 +41,3 @@ if __name__ == '__main__':
     part_1 = sum(list(item[0] for item in (filter(check_if_equation_possible, data))))
 
     print(part_1)
-
-
