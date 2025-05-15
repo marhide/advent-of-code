8ff9562cfc75f0093a3b4e64c1b13855dbb74bd0
marhide <louis.smith@hyperoceanic.com>
2025-05-15 23:42:06 +0100

    part 7 d1 progress

--- src/07_bridge_repair.py
+++ src/07_bridge_repair.py
@@ -1,24 +1,65 @@
+
+from pprint import pprint
+
+
+def get_all_operator_combinaton_list(maxLength, s=""):
+    if len(s) == maxLength:
+        return s
+    else:
+        temp = get_all_operator_combinaton_list(maxLength, s + "+") + "\n"
+        temp += get_all_operator_combinaton_list(maxLength, s + "*")
+        return temp
+    
 def check_if_equation_possible(item):
 
     result = item[0]
     equation = item[1]
     number_of_permuations = len(equation)**2
 
-    total = equation[0]
-    for i in range(1, number_of_permuations):
-        for number in equation[1::]:
-            total += number
 
+
+    # print(equation, allBinaryPossiblities(len(equation)-1))
+
+    operator_list = get_all_operator_combinaton_list(len(equation)-1).split('\n')
+    print(equation, operator_list)
+
+    # operations_list.append(equation)
+    total = 0
+    for i in range(len(operator_list)):
+        total = equation[0]
+        for j in range(len(equation[1::])):
+            if operator_list[i][j] == "+":
+                total += equation[j]
+            elif operator_list[i][j] == "*":
+                total *= equation[j]
         if total == result:
             print(total, result, number_of_permuations)
             return True
-
-    # a+b+c
-    # a*b+c
-    # a+b*c
-    # a*b*c
-    
+        
     return False
+
+    # for i in range(number_of_permuations):
+    #     new_list = []
+    #     total = equation[0]
+    #     for j in range(len(equation)):
+            # if j:
+            #     if i%j:
+            #         new_list.append('0')
+            #         total += equation[j]
+            #     else: 
+            #         new_list.append('1')
+            #         total *= equation[j]
+
+        # new_list.append('   '+str(i))
+
+
+        # operations_list.append(' '.join(new_list))
+
+
+
+
+    # pprint(operations_list)
+    # print(equation, len(set(operations_list)), len(operations_list))
 
 
 if __name__ == '__main__':
