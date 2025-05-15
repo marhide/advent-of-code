de3af45a000f29269c1f3830e3a4e0f7097b35ce
marhide <louis.smith@hyperoceanic.com>
2025-05-16 00:48:59 +0100

    refactor

--- src/07_bridge_repair.py
+++ src/07_bridge_repair.py
@@ -38,6 +38,5 @@ if __name__ == '__main__':
         data = [line.strip().split(': ') for line in file]
         data = [[int(item[0]), list(map(int, item[1].split(' ')))] for item in data]
 
-    part_1 = sum(list(item[0] for item in (filter(check_if_equation_possible, data))))
-
-    print(part_1)
+    part_1 = sum(map(lambda item: item[0], filter(check_if_equation_possible, data)))
+    print(f'part 1: {part_1}\npart 2: {""}')
