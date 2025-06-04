e6d98c12f8243eec722b46b011609dce8f0cc2c8
marhide <louis.smith@hyperoceanic.com>
2025-06-04 22:17:40 +0100

    refactor

--- src/08a_resonant_collinearity.py
+++ src/08a_resonant_collinearity.py
@@ -1,37 +1,35 @@
 def get_antinode_count(data):
     unique_chars = sorted(tuple(set(char for char in ''.join(data) if char != '.')))
     coords_dict = {char: [] for char in unique_chars}
-    y_range = range(1, len(data)+1)
-    x_range = range(1, len(data[0])+1)
+    coord_range = range(len(data))
  
-    for y in y_range:
-        for x in x_range:
-            if data[y-1][x-1] != '.':
-                coords_dict[data[y-1][x-1]].append((x, y))
+    for y in coord_range:
+        for x in coord_range:
+            if data[y][x] != '.':
+                coords_dict[data[y][x]].append((x, y))
 
     antinode_list = []
 
     for k in coords_dict:
-        # print(f'\n\n{k}: {coords_dict[k]}\n')
-        # new_map = [['.' for _ in x_range] for _ in y_range]
+        print(f'\n\n{k}: {coords_dict[k]}\n')
+        new_map = [['.' for _ in coord_range] for _ in coord_range]
         for i in range(len(coords_dict[k])):
             x, y = coords_dict[k][i]
-            # new_map[y][x] = k
+            new_map[y][x] = k
             for j in range(len(coords_dict[k])):
                 if i != j:
                     comparison_x, comparison_y = coords_dict[k][j]
                     x_antinode = x - (comparison_x - x)
                     y_antinode = y - (comparison_y - y)
 
-                    # print(f'i: {i}, j: {j} -- {x_antinode in x_range and y_antinode in y_range}\nx: {x}, comp x: {comparison_x}, x antinode: {x_antinode}, x antinode in range: {x_antinode in x_range}\ny: {y}, comp y: {comparison_y}, y antinode: {y_antinode}, y antinode in range: {y_antinode in y_range}\n')
+                    print(f'i: {i}, j: {j} -- {x_antinode in coord_range and y_antinode in coord_range}\nx: {x}, comp x: {comparison_x}, x antinode: {x_antinode}, x antinode in range: {x_antinode in coord_range}\ny: {y}, comp y: {comparison_y}, y antinode: {y_antinode}, y antinode in range: {y_antinode in coord_range}\n')
                     
-                    if x_antinode in x_range and y_antinode in y_range:
+                    if x_antinode in coord_range and y_antinode in coord_range:
                         antinode_list.append((x_antinode, y_antinode))
                         
-                        # new_map[y_antinode][x_antinode] = '#'
+                        new_map[y_antinode][x_antinode] = '#'
     
-        # new_map = [''.join(item) for item in new_map]
-        # for item in new_map: print(item)
+        print('\n'.join(''.join(item) for item in new_map))
     count = len(set(antinode_list))
     return count
 
--- src/08b_resonant_collinearity.py
+++ src/08b_resonant_collinearity.py
@@ -1,13 +1,10 @@
-from pprint import pprint
-
 def get_antinode_count(data):
     unique_chars = sorted(tuple(set(char for char in ''.join(data) if char != '.')))
     coords_dict = {char: [] for char in unique_chars}
-    y_range = range(1, len(data)+1)
-    x_range = range(1, len(data[0])+1)
+    coord_range = range(1, len(data)+1)
  
-    for y in y_range:
-        for x in x_range:
+    for y in coord_range:
+        for x in coord_range:
             if data[y-1][x-1] != '.':
                 coords_dict[data[y-1][x-1]].append((x, y))
 
@@ -15,7 +12,7 @@ def get_antinode_count(data):
 
     print_maps = []
     for k in coords_dict:
-        print_map = [['.' for _ in x_range] for _ in y_range]
+        print_map = [['.' for _ in coord_range] for _ in coord_range]
         for i in range(len(coords_dict[k])):
             x, y = coords_dict[k][i]
             print_map[y-1][x-1] = k
@@ -25,14 +22,14 @@ def get_antinode_count(data):
                     counter = 1
                     x_antinode = x - (comparison_x - x)
                     y_antinode = y - (comparison_y - y)
-                    while x_antinode in x_range and y_antinode in y_range:
+                    while x_antinode in coord_range and y_antinode in coord_range:
                         antinode_list.append((x_antinode, y_antinode))
                         print_map[y_antinode-1][x_antinode-1] = '#'
                         counter += 1
                         x_antinode = x - ((comparison_x - x)*counter)
                         y_antinode = y - ((comparison_y - y)*counter)
         print_maps.append('\n'.join([''.join(item) for item in print_map]))
-    with open('data/08_output.txt', 'w') as f: f.write('\n\n'.join(print_maps))
+    with open('data/08b_output.txt', 'w') as f: f.write('\n\n'.join(print_maps))
     count = len(set(antinode_list))
     return count
 
